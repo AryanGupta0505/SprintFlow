38a4bfc6cf644c2a8b391b6579fd337da9c4c5f4
AryanGupta0505 <aryan140505@gmail.com>
2026-03-06 02:40:26 +0530

    fix validation build export

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,17 +1,17 @@
-import { NextResponse } from "next/server"
-import prisma from "@repo/db/client";
+// import { NextResponse } from "next/server"
+// import prisma from "@repo/db/client";
 
 
-export const GET = async () => {
-    await prisma.user.create({
-        data: {
-            email: "asd",
-            name: "adsads",
-            number:'3333333333',
-            password:'asdasd'
-        }
-    })
-    return NextResponse.json({
-        message: "hi there"
-    })
-}
+// export const GET = async () => {
+//     await prisma.user.create({
+//         data: {
+//             email: "asd",
+//             name: "adsads",
+//             number:'3333333333',
+//             password:'asdasd'
+//         }
+//     })
+//     return NextResponse.json({
+//         message: "hi there"
+//     })
+// }
